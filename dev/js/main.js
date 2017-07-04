850cfad3add665d90b6bf825afe6c2199dbd06a5
konstantinkuchernko <konstantinkucherenko19@gmail.com>
2017-07-04 23:32:41 +0300

    fix_bag_js

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,14 +1,34 @@
 /*
 * @Author: admin
 * @Date:   2017-06-06 19:07:46
-* @Last Modified by:   admin
-* @Last Modified time: 2017-06-24 22:35:00
+* @Last Modified by:   konstantinkuchernko
+* @Last Modified time: 2017-07-04 22:50:11
 */
 
 ;(function($) {
 	"use strict";
-
+	
 	$(window).load(function() {
+
+		// INITIALIZE ANIMSITION
+		if($(".animsition").length){
+			$(".animsition").animsition({
+				inClass               :   'fade-in-up-sm',
+				outClass              :   'fade-out-up-sm',
+				inDuration            :    1100,
+				outDuration           :    800,
+				linkElement           :   '.animsition-link',
+				loading               :    true,
+				loadingParentElement  :   'body', 
+				unSupportCss          : [ 'animation-duration',
+										  '-webkit-animation-duration',
+										  '-o-animation-duration'
+										],
+				overlay               :   false,
+				overlayClass          :   'animsition-overlay-slie',
+				overlayParentElement  :   'body'
+			});
+		}
 
 		var portfolioGrid = $('.portfolio-grid');
 
